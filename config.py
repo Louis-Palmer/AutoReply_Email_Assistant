3b08576ae6a07f1a047f865b09e3700c03c08c11
Louis-Palmer <louispalmer01@outlook.com>
2025-07-23 18:42:07 +0100

    ref-wip: Added New assistant to config

--- config.py
+++ config.py
@@ -8,3 +8,4 @@ load_dotenv()
 # Keys and settings
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 OPENAI_ASSISTANT_KEY = os.getenv("OpenAI_Assistants_Key")
+OPENAI_ASSISTANT_CATEGORISE_KEY = os.getenv("OpenAI_Assistant_Categorise_Key")
